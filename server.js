53b17a9a083bc863dd16bc9d2472c12e8520f8f2
Zane Hitchcox <zwhitchcox@gmail.com>
2017-03-22 01:52:15 -0400

    init

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import Gun from '../..'
+import webpack from 'webpack'
+import WebpackDevMiddleware from 'webpack-dev-middleware'
+import config from './webpack.config'
+
+const app = express()
+const gun = Gun()
+
+Gun({web: app})
+
+const compiler = webpack(config)
+
+const devMiddleware = WebpackDevMiddleware(compiler)
+
+app.use(devMiddleware)
+
+app.listen(4000)
